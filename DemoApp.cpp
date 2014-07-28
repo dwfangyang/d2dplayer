77fed5feaea639a0699e8189c4ea8ac651a0132f
dwfangyang <jefffyang@qq.com>
2014-07-28 17:44:41 +0800

    d2dplayer开发依赖项
    开发所需lib,头文件及依赖的dlls

---
+++ DemoApp.cpp
@@ -0,0 +1,465 @@
+#include "DemoApp.h"
+#include <QtCore/QObject>
+#include <QtCore/QString>
+#include <QtCore/QChar>
+using namespace std;
+
+//#include <inttypes.h>  
+//#include <stdint.h>  
+
+
+struct buffer_data {
+	uint8_t *ptr;
+	size_t size; ///< size left in the buffer
+};
+
+static int read_packet(void *opaque, uint8_t *buf, int buf_size)
+{
+	struct buffer_data *bd = (struct buffer_data *)opaque;
+	buf_size = FFMIN(buf_size, int(bd->size));
+
+	printf("ptr:%p size:%zu\n", bd->ptr, bd->size);
+
+	/* copy internal buffer data to buf */
+	memcpy(buf, bd->ptr, buf_size);
+	bd->ptr  += buf_size;
+	bd->size -= buf_size;
+
+	return buf_size;
+}
+
+int WINAPI WinMain(	   HINSTANCE /* hInstance */,
+					  				   HINSTANCE /* hPrevInstance */,
+					  				   LPSTR  lpCmdLine ,
+					  				   int /* nCmdShow */)
+{
+		MessageBox(0,L"pause",0,0);
+		AVFormatContext *fmt_ctx = NULL;
+		AVIOContext *avio_ctx = NULL;
+		AVDictionaryEntry *tag = NULL;
+		uint8_t *buffer = NULL, *avio_ctx_buffer = NULL;
+		size_t buffer_size , avio_ctx_buffer_size = 4096;
+		char *input_filename = NULL;
+		int ret = 0;
+		struct buffer_data bd = { 0 };
+		
+		QString filename = lpCmdLine;
+		/*if (argc != 2) {
+			fprintf(stderr, "usage: %s input_file\n"
+				"API example program to show how to read from a custom buffer "
+				"accessed through AVIOContext.\n", argv[0]);
+			return 1;
+		}*/
+		if ( filename.isEmpty() )
+		{
+			MessageBox(0,L"empty filename",0,0);
+		}
+		
+		input_filename = lpCmdLine;
+
+		/* register codecs and formats and other lavf/lavc components*/
+		av_register_all();
+
+		/* slurp file content into buffer */
+		ret = av_file_map(input_filename, &buffer, &buffer_size, 0, NULL);
+		if (ret < 0)
+			goto end;
+
+		/* fill opaque structure used by the AVIOContext read callback */
+		bd.ptr  = buffer;
+		bd.size = buffer_size;
+
+		if (!(fmt_ctx = avformat_alloc_context())) {
+			ret = AVERROR(ENOMEM);
+			goto end;
+		}
+
+		avio_ctx_buffer = (uint8_t*)(av_malloc(avio_ctx_buffer_size));
+		if (!avio_ctx_buffer) {
+			ret = AVERROR(ENOMEM);
+			goto end;
+		}
+		avio_ctx = avio_alloc_context(avio_ctx_buffer, avio_ctx_buffer_size,
+			0, &bd, &read_packet, NULL, NULL);
+		if (!avio_ctx) {
+			ret = AVERROR(ENOMEM);
+			goto end;
+		}
+		fmt_ctx->pb = avio_ctx;
+
+		ret = avformat_open_input(&fmt_ctx, NULL, NULL, NULL);
+		if (ret < 0) {
+			//fprintf(stderr, "Could not open input\n");
+			goto end;
+		}
+
+		ret = avformat_find_stream_info(fmt_ctx, NULL);
+		if (ret < 0) {
+			//fprintf(stderr, "Could not find stream information\n");
+			goto end; 
+		}
+
+		av_dump_format(fmt_ctx, 0, input_filename, 0);
+		while ((tag = av_dict_get(fmt_ctx->metadata, "", tag, AV_DICT_IGNORE_SUFFIX)))
+			printf("%s=%s\n", tag->key, tag->value);
+
+	end:
+		avformat_close_input(&fmt_ctx);
+		/* note: the internal buffer could have changed, and be != avio_ctx_buffer */
+		if (avio_ctx) {
+			av_freep(&avio_ctx->buffer);
+			av_freep(&avio_ctx);
+		}
+		av_file_unmap(buffer, buffer_size);
+
+		if (ret < 0) 
+		{
+			//fprintf(stderr, "Error occurred: %s\n", av_err2str(ret));
+			MessageBox(0,QString("error: %1").arg(ret).toStdWString().c_str(),0,0);
+			return 1;
+		}
+	return 0;	
+}
+
+
+
+//DemoApp::DemoApp() :
+//m_hwnd(NULL),
+//m_pDirect2dFactory(NULL),
+//m_pRenderTarget(NULL),
+//m_pLightSlateGrayBrush(NULL),
+//m_pCornflowerBlueBrush(NULL)
+//{
+//}
+//DemoApp::~DemoApp()
+//{
+//	SafeRelease(&m_pDirect2dFactory);
+//	SafeRelease(&m_pRenderTarget);
+//	SafeRelease(&m_pLightSlateGrayBrush);
+//	SafeRelease(&m_pCornflowerBlueBrush);
+//
+//}
+//
+//void DemoApp::RunMessageLoop()
+//{
+//	MSG msg;
+//
+//	while (GetMessage(&msg, NULL, 0, 0))
+//	{
+//		TranslateMessage(&msg);
+//		DispatchMessage(&msg);
+//	}
+//}
+//
+//HRESULT DemoApp::Initialize()
+//{
+//	HRESULT hr;
+//
+//	// Initialize device-indpendent resources, such
+//	// as the Direct2D factory.
+//	hr = CreateDeviceIndependentResources();
+//
+//	if (SUCCEEDED(hr))
+//	{
+//		// Register the window class.
+//		WNDCLASSEX wcex = { sizeof(WNDCLASSEX) };
+//		wcex.style         = CS_HREDRAW | CS_VREDRAW;
+//		wcex.lpfnWndProc   = DemoApp::WndProc;
+//		wcex.cbClsExtra    = 0;
+//		wcex.cbWndExtra    = sizeof(LONG_PTR);
+//		wcex.hInstance     = HINST_THISCOMPONENT;
+//		wcex.hbrBackground = NULL;
+//		wcex.lpszMenuName  = NULL;
+//		wcex.hCursor       = LoadCursor(NULL, IDI_APPLICATION);
+//		wcex.lpszClassName = L"D2DDemoApp";
+//
+//		RegisterClassEx(&wcex);
+//
+//
+//		// Because the CreateWindow function takes its size in pixels,
+//		// obtain the system DPI and use it to scale the window size.
+//		FLOAT dpiX, dpiY;
+//
+//		// The factory returns the current system DPI. This is also the value it will use
+//		// to create its own windows.
+//		m_pDirect2dFactory->GetDesktopDpi(&dpiX, &dpiY);
+//		//MessageBox(0,QString("dpix:%1,dpiy:%2").arg(dpiX).arg(dpiY).toStdWString().c_str(),0,0);		//±¾»úÅÜÎª96£¬96
+//
+//		// Create the window.
+//		m_hwnd = CreateWindow(
+//			L"D2DDemoApp",
+//			L"Direct2D Demo App",
+//			WS_OVERLAPPEDWINDOW,
+//			CW_USEDEFAULT,
+//			CW_USEDEFAULT,
+//			static_cast<UINT>(ceil(640.f * dpiX / 96.f)),
+//			static_cast<UINT>(ceil(480.f * dpiY / 96.f)),
+//			NULL,
+//			NULL,
+//			HINST_THISCOMPONENT,
+//			this
+//			);
+//		hr = m_hwnd ? S_OK : E_FAIL;
+//		if (SUCCEEDED(hr))
+//		{
+//			ShowWindow(m_hwnd, SW_SHOWNORMAL);
+//			UpdateWindow(m_hwnd);
+//		}
+//	}
+//
+//	return hr;
+//}
+//
+//HRESULT DemoApp::CreateDeviceIndependentResources()
+//{
+//	HRESULT hr = S_OK;
+//
+//	// Create a Direct2D factory.
+//	hr = D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, &m_pDirect2dFactory);
+//
+//	return hr;
+//}
+//
+//HRESULT DemoApp::CreateDeviceResources()
+//{
+//	HRESULT hr = S_OK;
+//
+//	if (!m_pRenderTarget)
+//	{
+//		RECT rc;
+//		GetClientRect(m_hwnd, &rc);
+//
+//		D2D1_SIZE_U size = D2D1::SizeU(
+//			rc.right - rc.left,
+//			rc.bottom - rc.top
+//			);
+//
+//		// Create a Direct2D render target.
+//		hr = m_pDirect2dFactory->CreateHwndRenderTarget(
+//			D2D1::RenderTargetProperties(),
+//			D2D1::HwndRenderTargetProperties(m_hwnd, size),
+//			&m_pRenderTarget
+//			);
+//
+//
+//		if (SUCCEEDED(hr))
+//		{
+//			// Create a gray brush.
+//			hr = m_pRenderTarget->CreateSolidColorBrush(
+//				D2D1::ColorF(D2D1::ColorF::LightSlateGray),
+//				&m_pLightSlateGrayBrush
+//				);
+//		}
+//		if (SUCCEEDED(hr))
+//		{
+//			// Create a blue brush.
+//			hr = m_pRenderTarget->CreateSolidColorBrush(
+//				D2D1::ColorF(D2D1::ColorF::CornflowerBlue),
+//				&m_pCornflowerBlueBrush
+//				);
+//		}
+//	}
+//
+//	return hr;
+//}
+//
+//void DemoApp::DiscardDeviceResources()
+//{
+//	SafeRelease(&m_pRenderTarget);
+//	SafeRelease(&m_pLightSlateGrayBrush);
+//	SafeRelease(&m_pCornflowerBlueBrush);
+//}
+//
+//LRESULT CALLBACK DemoApp::WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+//{
+//	LRESULT result = 0;
+//
+//	if (message == WM_CREATE)
+//	{
+//		LPCREATESTRUCT pcs = (LPCREATESTRUCT)lParam;
+//		DemoApp *pDemoApp = (DemoApp *)pcs->lpCreateParams;
+//
+//		::SetWindowLongPtrW(
+//			hwnd,
+//			GWLP_USERDATA,
+//			PtrToUlong(pDemoApp)
+//			);
+//
+//		result = 1;
+//	}
+//	else
+//	{
+//		DemoApp *pDemoApp = reinterpret_cast<DemoApp *>(static_cast<LONG_PTR>(
+//			::GetWindowLongPtrW(
+//			hwnd,
+//			GWLP_USERDATA
+//			)));
+//
+//		bool wasHandled = false;
+//
+//		if (pDemoApp)
+//		{
+//			switch (message)
+//			{
+//			case WM_SIZE:
+//				{
+//					UINT width = LOWORD(lParam);
+//					UINT height = HIWORD(lParam);
+//					pDemoApp->OnResize(width, height);
+//				}
+//				result = 0;
+//				wasHandled = true;
+//				break;
+//
+//			case WM_DISPLAYCHANGE:
+//				{
+//					InvalidateRect(hwnd, NULL, FALSE);
+//				}
+//				result = 0;
+//				wasHandled = true;
+//				break;
+//
+//			case WM_PAINT:
+//				{
+//					pDemoApp->OnRender();
+//					ValidateRect(hwnd, NULL);
+//				}
+//				result = 0;
+//				wasHandled = true;
+//				break;
+//
+//			case WM_DESTROY:
+//				{
+//					PostQuitMessage(0);
+//				}
+//				result = 1;
+//				wasHandled = true;
+//				break;
+//			}
+//		}
+//
+//		if (!wasHandled)
+//		{
+//			result = DefWindowProc(hwnd, message, wParam, lParam);
+//		}
+//	}
+//
+//	return result;
+//}
+//
+//void DemoApp::OnResize(UINT width, UINT height)
+//{
+//	if (m_pRenderTarget)
+//	{
+//		// Note: This method can fail, but it's okay to ignore the
+//		// error here, because the error will be returned again
+//		// the next time EndDraw is called.
+//		m_pRenderTarget->Resize(D2D1::SizeU(width, height));
+//	}
+//}
+//
+//HRESULT DemoApp::OnRender()
+//{
+//	HRESULT hr = S_OK;
+//
+//	hr = CreateDeviceResources();
+//
+//	if (SUCCEEDED(hr))
+//	{
+//		m_pRenderTarget->BeginDraw();
+//
+//		m_pRenderTarget->SetTransform(D2D1::Matrix3x2F::Identity());
+//
+//		m_pRenderTarget->Clear(D2D1::ColorF(D2D1::ColorF::White));
+//
+//		D2D1_SIZE_F rtSize = m_pRenderTarget->GetSize();
+//		// Draw a grid background.
+//		int width = static_cast<int>(rtSize.width);
+//		int height = static_cast<int>(rtSize.height);
+//
+//		for (int x = 0; x < width; x += 10)
+//		{
+//			m_pRenderTarget->DrawLine(
+//				D2D1::Point2F(static_cast<FLOAT>(x), 0.0f),
+//				D2D1::Point2F(static_cast<FLOAT>(x), rtSize.height),
+//				m_pLightSlateGrayBrush,
+//				0.2f
+//				);
+//		}
+//
+//		for (int y = 0; y < height; y += 10)
+//		{
+//			m_pRenderTarget->DrawLine(
+//				D2D1::Point2F(0.0f, static_cast<FLOAT>(y)),
+//				D2D1::Point2F(rtSize.width, static_cast<FLOAT>(y)),
+//				m_pLightSlateGrayBrush,
+//				0.2f
+//				);
+//		}
+//
+//		// Draw two rectangles.
+//		D2D1_RECT_F rectangle1 = D2D1::RectF(
+//			rtSize.width/2 - 50.0f,
+//			rtSize.height/2 - 50.0f,
+//			rtSize.width/2 + 50.0f,
+//			rtSize.height/2 + 50.0f
+//			);
+//
+//		D2D1_RECT_F rectangle2 = D2D1::RectF(
+//			rtSize.width/2 - 100.0f,
+//			rtSize.height/2 - 100.0f,
+//			rtSize.width/2 + 100.0f,
+//			rtSize.height/2 + 100.0f
+//			);
+//
+//		// Draw a filled rectangle.
+//		m_pRenderTarget->FillRectangle(&rectangle1, m_pLightSlateGrayBrush);
+//
+//		// Draw the outline of a rectangle.
+//		m_pRenderTarget->DrawRectangle(&rectangle2, m_pCornflowerBlueBrush);
+//
+//		hr = m_pRenderTarget->EndDraw();
+//	}
+//
+//	if (hr == D2DERR_RECREATE_TARGET)
+//	{
+//		hr = S_OK;
+//		DiscardDeviceResources();
+//	}
+//
+//	return hr;
+//}
+//
+//int WINAPI WinMain(
+//				   HINSTANCE /* hInstance */,
+//				   HINSTANCE /* hPrevInstance */,
+//				   LPSTR /* lpCmdLine */,
+//				   int /* nCmdShow */
+//				   )
+//{
+//	// Use HeapSetInformation to specify that the process should
+//	// terminate if the heap manager detects an error in any heap used
+//	// by the process.
+//	// The return value is ignored, because we want to continue running in the
+//	// unlikely event that HeapSetInformation fails.
+//	HeapSetInformation(NULL, HeapEnableTerminationOnCorruption, NULL, 0);
+//	HMODULE ab = LoadLibrary(L"D:\\duowan\\yydev\\yy\\dev572067\\dwutility.dll");
+//	if ( !ab )
+//	{
+//		//MessageBox(0,QString("wrong:%1").arg(GetLastError()).toStdWString().c_str(),0,0);
+//	}
+//
+//	if (SUCCEEDED(CoInitialize(NULL)))
+//	{
+//		{
+//			DemoApp app;
+//
+//			if (SUCCEEDED(app.Initialize()))
+//			{
+//				app.RunMessageLoop();
+//			}
+//		}
+//		CoUninitialize();
+//	}
+//	return 0;
+//}
---
+++ DemoApp.h
@@ -0,0 +1,110 @@
+#include <Windows.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <wchar.h>
+#include <math.h>
+#include <d2d1.h>
+#include <d2d1helper.h>
+#include <dwrite.h>
+#include <wincodec.h>
+#include <string>
+
+#ifdef __cplusplus
+#define __STDC_CONSTANT_MACROS
+#ifdef _STDINT_H
+#undef _STDINT_H
+#endif
+#endif
+
+#ifndef INT64_C													//应对UINT64_C未定义
+#define INT64_C(c) (c ## LL)
+#define UINT64_C(c) (c ## ULL)
+#endif
+
+#ifdef __cplusplus  
+extern "C"   
+{  
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libavformat/avio.h>
+#include <libavutil/file.h>
+#include <libavutil/dict.h>
+}  
+#endif 
+
+template<class Interface>
+inline void SafeRelease(
+						Interface **ppInterfaceToRelease
+						)
+{
+	if (*ppInterfaceToRelease != NULL)
+	{
+		(*ppInterfaceToRelease)->Release();
+
+		(*ppInterfaceToRelease) = NULL;
+	}
+}
+
+
+#ifndef Assert
+#if defined( DEBUG ) || defined( _DEBUG )
+#define Assert(b) do {if (!(b)) {OutputDebugStringA("Assert: " #b "\n");}} while(0)
+#else
+#define Assert(b)
+#endif //DEBUG || _DEBUG
+#endif
+
+
+
+#ifndef HINST_THISCOMPONENT
+EXTERN_C IMAGE_DOS_HEADER __ImageBase;
+#define HINST_THISCOMPONENT ((HINSTANCE)&__ImageBase)
+#endif
+
+
+class DemoApp
+{
+public:
+	DemoApp();
+	~DemoApp();
+
+	// Register the window class and call methods for instantiating drawing resources
+	HRESULT Initialize();
+
+	// Process and dispatch messages
+	void RunMessageLoop();
+
+private:
+	// Initialize device-independent resources.
+	HRESULT CreateDeviceIndependentResources();
+
+	// Initialize device-dependent resources.
+	HRESULT CreateDeviceResources();
+
+	// Release device-dependent resource.
+	void DiscardDeviceResources();
+
+	// Draw content.
+	HRESULT OnRender();
+
+	// Resize the render target.
+	void OnResize(
+		UINT width,
+		UINT height
+		);
+
+	// The windows procedure.
+	static LRESULT CALLBACK WndProc(
+		HWND hWnd,
+		UINT message,
+		WPARAM wParam,
+		LPARAM lParam
+		);
+private:
+	HWND m_hwnd;
+	ID2D1Factory* m_pDirect2dFactory;
+	ID2D1HwndRenderTarget* m_pRenderTarget;
+	ID2D1SolidColorBrush* m_pLightSlateGrayBrush;
+	ID2D1SolidColorBrush* m_pCornflowerBlueBrush;
+};
